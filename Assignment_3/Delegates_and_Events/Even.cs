123a0e68b8329f24dce68bd617d03eae692580d8
sreelakshmi587 <90371930+sreelakshmi587@users.noreply.github.com>
2022-01-03 11:30:44 +0530

    Add files via upload

---
+++ Assignment_3/Delegates_and_Events/Even.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void Mydel();
+    class Even
+    {
+        public event Mydel Programexecuted;
+        public void Findeven()
+        {
+            Console.WriteLine( "Printing even numbers");
+            for(int num = 1; num <= 100; num++)
+            {
+               
+                if (num%2==0)
+                    Console.WriteLine(num); 
+            }
+            OnProgramexecuted();
+        }
+
+        protected virtual void OnProgramexecuted()
+        {
+            Programexecuted?.Invoke();
+        }
+    }
+}
---
+++ Assignment_3/Delegates_and_Events/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Even even = new Even();
+            even.Programexecuted += even_Programexecuted;
+            even.Findeven();
+        }
+
+        private static void even_Programexecuted()
+        {
+            Console.WriteLine("Printed the required Even numbers!");
+        }
+    }
+}
